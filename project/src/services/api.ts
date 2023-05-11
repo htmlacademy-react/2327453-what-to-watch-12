f9a72db1d6213fe0046a1c924da0c1cfa7365923
Stepan Grankin <sgrankin@inbox.ru>
2023-05-11 10:06:23 +0300

    added api service

---
+++ project/src/services/api.ts
@@ -0,0 +1,14 @@
+﻿import axios, {AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse} from 'axios';
+
+const BACKEND_URL = 'https://12.react.pages.academy/wtw';
+const REQUEST_TIMEOUT = 5000;
+const TOKEN_HEADER_NAME = 'x-token';
+
+export const createAPI = (): AxiosInstance => {
+  const api = axios.create({
+    baseURL: BACKEND_URL,
+    timeout: REQUEST_TIMEOUT
+  });
+
+  return api;
+};
