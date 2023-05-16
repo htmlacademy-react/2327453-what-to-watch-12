fb5a6ef8434e0dd7a1706b55b2b1685c13210374
Stepan Grankin <sgrankin@inbox.ru>
2023-05-16 21:59:37 +0300

    fixes by linter

--- project/src/types/film.ts
+++ project/src/types/film.ts
@@ -1,19 +1,19 @@
 export type Film = {
-  name: string,
-  posterImage: string,
-  previewImage: string,
-  backgroundImage: string,
-  backgroundColor: string,
-  description: string,
-  rating: number,
-  scoresCount: number,
-  director: string,
-  starring: string[],
-  runTime: number,
-  genre: string,
-  released: number,
-  id: number,
-  isFavorite: boolean,
-  videoLink: string,
-  previewVideoLink: string
+  name: string;
+  posterImage: string;
+  previewImage: string;
+  backgroundImage: string;
+  backgroundColor: string;
+  description: string;
+  rating: number;
+  scoresCount: number;
+  director: string;
+  starring: string[];
+  runTime: number;
+  genre: string;
+  released: number;
+  id: number;
+  isFavorite: boolean;
+  videoLink: string;
+  previewVideoLink: string;
 };
