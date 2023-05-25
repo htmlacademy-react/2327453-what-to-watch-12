a22901c1bb359c01131f64ed89a686ad944df8cf
Stepan Grankin <sgrankin@inbox.ru>
2023-05-25 09:31:05 +0300

    PrivateRoute

---
+++ project/src/components/private-route/private-route.tsx
@@ -0,0 +1,8 @@
+﻿import {useAppSelector} from "../../hooks";
+import {Navigate} from "react-router-dom";
+import {AppRoute} from "../../const";
+
+export default function PrivateRoute(children: JSX.Element): JSX.Element {
+  const isUserAuthorized = useAppSelector((state) => state.isUserAuthorized);
+  return (isUserAuthorized ? children : <Navigate to={AppRoute.SignIn}/>);
+}
