0d3b7a3ebaff74f120884837370f5f9e98340d6d
Stepan Grankin <sgrankin@inbox.ru>
2023-05-25 09:39:22 +0300

    Routes

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,8 +1,19 @@
 import Main from '../../pages/main/main';
+import React from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {AppRoute} from "../../const";
+import SignIn from "../../pages/sign-in/sign-in";
 
 function App(): JSX.Element {
   return (
-    <Main />
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Main}>
+          <Route index element={<Main/>}/>
+          <Route path={AppRoute.SignIn} element={<SignIn/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
