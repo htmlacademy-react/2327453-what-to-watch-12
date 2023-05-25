a50d0d04edd987222618908d244507fb55867d87
Stepan Grankin <sgrankin@inbox.ru>
2023-05-25 09:18:49 +0300

    AppRoute const

--- project/src/const.ts
+++ project/src/const.ts
@@ -1,3 +1,11 @@
 export enum APIRoute {
   Promo = '/promo'
 }
+
+export enum AppRoute {
+  Main = '/',
+  SignIn = '/login',
+  Film = '/films/:id',
+  AddReview = '/films/:id/review',
+  Player = '/player/:id'
+}
