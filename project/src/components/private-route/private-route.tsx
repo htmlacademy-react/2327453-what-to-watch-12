d4d7b5b816d9406f42bc461e49d4150ffabca285
Stepan Grankin <sgrankin@inbox.ru>
2023-05-29 09:50:00 +0300

    added privateRouteProps type

--- project/src/components/private-route/private-route.tsx
+++ project/src/components/private-route/private-route.tsx
@@ -2,7 +2,12 @@
 import {Navigate} from "react-router-dom";
 import {AppRoute} from "../../const";
 
-export default function PrivateRoute(children: JSX.Element): JSX.Element {
+type privateRouteProps = {
+  children: JSX.Element
+}
+
+export default function PrivateRoute(props: privateRouteProps): JSX.Element {
+  const {children} = props;
   const isUserAuthorized = useAppSelector((state) => state.isUserAuthorized);
   return (isUserAuthorized ? children : <Navigate to={AppRoute.SignIn}/>);
 }
