0259a7ff5674b898f8a293a4f1a8eecdf3746163
Stepan Grankin <sgrankin@inbox.ru>
2023-05-31 09:35:25 +0300

    SmallFilmCardProps and link to film page

--- project/src/components/small-film-card/small-film-card.tsx
+++ project/src/components/small-film-card/small-film-card.tsx
@@ -1,11 +1,21 @@
-export default function SmallFilmCard(): JSX.Element {
+import {Film} from '../../types/film';
+import {AppRoute} from '../../const';
+import {generatePath, Link} from 'react-router-dom';
+
+type SmallFilmCardProps = {
+  film: Film;
+}
+
+export default function SmallFilmCard(props: SmallFilmCardProps): JSX.Element {
+  const {film} = props;
+
   return (
     <article className="small-film-card catalog__films-card">
       <div className="small-film-card__image">
-        <img src="img/fantastic-beasts-the-crimes-of-grindelwald.jpg" alt="Fantastic Beasts: The Crimes of Grindelwald" width="280" height="175" />
+        <img src={film.previewImage} alt={film.name} width="280" height="175" />
       </div>
       <h3 className="small-film-card__title">
-        <a className="small-film-card__link" href="film-page.html">Fantastic Beasts: The Crimes of Grindelwald</a>
+        <Link className="small-film-card__link" to={generatePath(AppRoute.Film, {id : film.id.toString()})}>{film.name}</Link>
       </h3>
     </article>
   );
