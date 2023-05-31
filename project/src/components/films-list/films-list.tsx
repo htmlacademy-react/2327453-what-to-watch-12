1c2d0d4354f3f44cb4e277639daa42b01c8aaf5f
Stepan Grankin <sgrankin@inbox.ru>
2023-05-31 09:37:44 +0300

    used loaded films at FilmsList

--- project/src/components/films-list/films-list.tsx
+++ project/src/components/films-list/films-list.tsx
@@ -1,20 +1,28 @@
 import SmallFilmCard from '../small-film-card/small-film-card';
+import {store} from '../../store';
+import {loadFilms} from '../../store/api-actions';
+import {useAppSelector} from '../../hooks';
+import {Films} from '../../types/film';
 
-function addSmallFilmCard(count:number): JSX.Element[]
+function addSmallFilmCard(films: Films, count: number): JSX.Element[]
 {
   const result: JSX.Element[] = [];
   for(let i = 0; i < count; i++)
   {
-    result.push(<SmallFilmCard />);
+    const film = films[i];
+    result.push(<SmallFilmCard film={film} />);
 
   }
   return result;
 }
 
+store.dispatch(loadFilms());
+
 export default function FilmsList(): JSX.Element {
+  const films = useAppSelector((state) => state.films);
   return (
     <div className="catalog__films-list">
-      {addSmallFilmCard(20)}
+      {addSmallFilmCard(films, 20)}
     </div>
   );
 }
