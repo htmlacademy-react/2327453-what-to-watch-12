56b6c150f62e349bcee60c40b83bca096d0c23d6
Stepan Grankin <sgrankin@inbox.ru>
2023-06-01 17:43:25 +0300

    inline mapping from films to elements

--- project/src/components/films-list/films-list.tsx
+++ project/src/components/films-list/films-list.tsx
@@ -2,23 +2,6 @@ import SmallFilmCard from '../small-film-card/small-film-card';
 import {store} from '../../store';
 import {loadFilms} from '../../store/api-actions';
 import {useAppSelector} from '../../hooks';
-import {Films} from '../../types/film';
-
-function addSmallFilmCard(films: Films, count: number): JSX.Element[]
-{
-  const result: JSX.Element[] = [];
-  for(let i = 0; i < count; i++)
-  {
-    const film = films[i];
-    if (!film)
-    {
-      continue;
-    }
-    result.push(<SmallFilmCard film={film} key={film.id}/>);
-
-  }
-  return result;
-}
 
 store.dispatch(loadFilms());
 
@@ -26,7 +9,13 @@ export default function FilmsList(): JSX.Element {
   const films = useAppSelector((state) => state.films);
   return (
     <div className="catalog__films-list">
-      {addSmallFilmCard(films, 20)}
+      {
+        films
+          ?
+          films.slice(0, 8).map<JSX.Element>((f) => <SmallFilmCard film={f} key={f.id}/>)
+          :
+          <>No any films</>
+      }
     </div>
   );
 }
