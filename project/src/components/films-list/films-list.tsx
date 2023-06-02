56456298309253062e4fce91fef2c3e95d014656
Stepan Grankin <sgrankin@inbox.ru>
2023-06-02 09:24:26 +0300

    mouse events at FilmsList

--- project/src/components/films-list/films-list.tsx
+++ project/src/components/films-list/films-list.tsx
@@ -4,16 +4,23 @@ import {loadFilms} from '../../store/api-actions';
 import {useAppSelector} from '../../hooks';
 import {Settings} from '../../const';
 
+type FilmsListProps = {
+  onMouseEnter(filmId: number): void;
+  onMouseLeave(): void;
+}
+
 store.dispatch(loadFilms());
 
-export default function FilmsList(): JSX.Element {
+export default function FilmsList({onMouseEnter, onMouseLeave}: FilmsListProps): JSX.Element {
   const films = useAppSelector((state) => state.films);
   return (
     <div className="catalog__films-list">
       {
         films
           ?
-          films.slice(0, Settings.MaxFilmsAtList).map<JSX.Element>((f) => <SmallFilmCard film={f} key={f.id}/>)
+          films.slice(0, Settings.MaxFilmsAtList).map<JSX.Element>((f) =>
+            <SmallFilmCard film={f} key={f.id} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} />
+          )
           :
           <>No any films</>
       }
