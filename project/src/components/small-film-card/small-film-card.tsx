7dda078b1172b76d9140d77f7cf421765f7d00a2
Stepan Grankin <sgrankin@inbox.ru>
2023-06-02 09:20:52 +0300

    mouse events at SmallFilmCard

--- project/src/components/small-film-card/small-film-card.tsx
+++ project/src/components/small-film-card/small-film-card.tsx
@@ -4,13 +4,16 @@ import {generatePath, Link} from 'react-router-dom';
 
 type SmallFilmCardProps = {
   film: Film;
+  onMouseEnter(filmId: number): void;
+  onMouseLeave(): void;
 }
 
-export default function SmallFilmCard(props: SmallFilmCardProps): JSX.Element {
-  const {film} = props;
-
+export default function SmallFilmCard({film, onMouseEnter, onMouseLeave}: SmallFilmCardProps): JSX.Element {
   return (
-    <article className="small-film-card catalog__films-card">
+    <article className="small-film-card catalog__films-card"
+             onMouseEnter={() => onMouseEnter(film.id)}
+             onMouseLeave={() => onMouseLeave()}
+    >
       <Link className="small-film-card__link" to={generatePath(AppRoute.Film, {id : film.id.toString()})}>
         <div className="small-film-card__image">
           <img src={film.previewImage} alt={film.name} width="280" height="175" />
