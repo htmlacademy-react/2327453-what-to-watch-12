26a6d805e5cb959b4fedffafe4aba00ca217df5d
Stepan Grankin <sgrankin@inbox.ru>
2023-06-05 10:14:42 +0300

    fixes by linter

--- project/src/pages/add-review/add-review.tsx
+++ project/src/pages/add-review/add-review.tsx
@@ -1,4 +1,4 @@
-import {ChangeEvent, useState} from "react";
+import {ChangeEvent, useState} from 'react';
 
 export default function AddReview(): JSX.Element {
   const [review, setReview] = useState({});
