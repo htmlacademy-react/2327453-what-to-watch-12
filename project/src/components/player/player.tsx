e3f6f80bdc55b595a43cf2c6a49acd8e9245db8f
Stepan Grankin <sgrankin@inbox.ru>
2023-06-06 17:39:20 +0300

    added Player component

---
+++ project/src/components/player/player.tsx
@@ -0,0 +1,35 @@
+﻿import { useEffect, useRef } from 'react';
+
+type CardPlayerProps = {
+  preview: string;
+  trailer: string | undefined;
+  play: boolean;
+};
+
+export default function Player({ preview, trailer, play }: CardPlayerProps): JSX.Element {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    const videoElement = videoRef.current;
+
+    if(!videoElement)
+    {
+      return;
+    }
+
+    if (play) {
+      const timeout = setTimeout(() => {
+        videoElement.play();
+      }, 1000);
+      return () => clearTimeout(timeout);
+    } else {
+      videoElement.load();
+    }
+  }, [play]);
+
+  return (
+    <video ref={videoRef} className="small-film-card__image" muted loop poster={preview}>
+      <source src={trailer} />
+    </video>
+  );
+}
