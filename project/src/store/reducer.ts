15d1510c108a15fe57c8be7d41a231e0b7f55ae1
Stepan Grankin <sgrankin@inbox.ru>
2023-06-19 18:30:55 +0300

    reviews at state

--- project/src/store/reducer.ts
+++ project/src/store/reducer.ts
@@ -1,16 +1,19 @@
 import { createReducer } from '@reduxjs/toolkit';
 import {Film, Films} from '../types/film';
 import {filmsLoaded, promoLoaded} from './action';
+import {Reviews} from "../types/review";
 
 type state = {
   promo: Film | null;
   films: Films;
+  reviews: Reviews;
   isUserAuthorized: boolean;
 }
 
 const initialState: state = {
   promo: null,
   films: [],
+  reviews: [],
   isUserAuthorized: false
 };
 
