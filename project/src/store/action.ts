30d3c7dbb30e281fe0d23e0f19f1217e9deae9f4
Stepan Grankin <sgrankin@inbox.ru>
2023-06-19 18:29:32 +0300

    reviewsLoaded action

--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -1,5 +1,7 @@
 import {createAction} from '@reduxjs/toolkit';
 import {Film, Films} from '../types/film';
+import {Reviews} from "../types/review";
 
 export const promoLoaded = createAction<Film>('promoLoaded');
 export const filmsLoaded = createAction<Films>('filmsLoaded');
+export const reviewsLoaded = createAction<Reviews>('reviewsLoaded');
