3dbff31319f274a4809f371492744efa9cf4f978
Stepan Grankin <sgrankin@inbox.ru>
2023-06-19 18:28:45 +0300

    review type

---
+++ project/src/types/review.ts
@@ -0,0 +1,11 @@
+﻿import {User} from "./user";
+
+export type Review = {
+  id: number;
+  user: User,
+  rating: number,
+  "comment": string,
+  "date": Date
+};
+
+export type Reviews = Review[];
