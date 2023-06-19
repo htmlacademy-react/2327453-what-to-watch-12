4388800d9bd07924506b32a1031769bbc4d12dd0
Stepan Grankin <sgrankin@inbox.ru>
2023-06-19 18:12:26 +0300

    getRatingDescription

---
+++ project/src/types/number-extensions.ts
@@ -0,0 +1,22 @@
+﻿export function getRatingDescription(rating: number): string {
+  switch (rating) {
+    case 0:
+    case 1:
+    case 2:
+      return 'Bad';
+    case 3:
+    case 4:
+      return 'Very good';
+    case 5:
+    case 6:
+      return 'Very good';
+    case 7:
+    case 8:
+      return 'Very good';
+    case 9:
+    case 10:
+      return 'Awesome';
+    default:
+      return 'Incredible!';
+  }
+}
