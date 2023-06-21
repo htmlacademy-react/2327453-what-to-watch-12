069068ee9ad1a46945903f3e3cbb9cd12bb33774
Stepan Grankin <sgrankin@inbox.ru>
2023-06-21 17:27:47 +0300

    fixes by linter

--- project/src/components/reviews/reviews.tsx
+++ project/src/components/reviews/reviews.tsx
@@ -2,7 +2,7 @@ import {useEffect} from 'react';
 import {loadReviews} from '../../store/api-actions';
 import {store} from '../../store';
 import {useAppSelector} from '../../hooks';
-import {formatDate} from "../../types/date-extensions";
+import {formatDate} from '../../types/date-extensions';
 
 type overviewProps = {
   filmId: number | undefined;
@@ -20,7 +20,7 @@ export default function Reviews({filmId}: overviewProps): JSX.Element {
   return (
     <div className="film-card__reviews film-card__row">
       <div className="film-card__reviews-col">
-        {reviews.map(review => (
+        {reviews.map((review) => (
           <div className="review" key={review.id}>
             <blockquote className="review__quote">
               <p className="review__text">{review.comment}</p>
--- project/src/types/date-extensions.ts
+++ project/src/types/date-extensions.ts
@@ -1,4 +1,4 @@
-﻿import moment from "moment";
+import moment from 'moment';
 
 export function formatDate(date: Date, format: string): string {
   return moment(date).format(format);
