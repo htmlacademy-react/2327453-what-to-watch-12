d2b7ae553fd4150b3bf187e2d6fa02caed4c8a3c
Stepan Grankin <sgrankin@inbox.ru>
2023-06-22 17:52:03 +0300

    show info about choosed film

--- project/src/pages/movie-page/movie-page.tsx
+++ project/src/pages/movie-page/movie-page.tsx
@@ -12,10 +12,10 @@ export default function MoviePage(): JSX.Element {
   const film = films.find((f) => f.id.toString() === id);
   return (
     <>
-      <section className="film-card film-card--full">
+      <section className="film-card film-card--full" style={{backgroundColor: film?.backgroundColor}}>
         <div className="film-card__hero">
           <div className="film-card__bg">
-            <img src="img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel" />
+            <img src={film?.backgroundImage} alt={film?.name} />
           </div>
 
           <h1 className="visually-hidden">WTW</h1>
@@ -43,10 +43,10 @@ export default function MoviePage(): JSX.Element {
 
           <div className="film-card__wrap">
             <div className="film-card__desc">
-              <h2 className="film-card__title">The Grand Budapest Hotel</h2>
+              <h2 className="film-card__title">{film?.name}</h2>
               <p className="film-card__meta">
-                <span className="film-card__genre">Drama</span>
-                <span className="film-card__year">2014</span>
+                <span className="film-card__genre">{film?.genre}</span>
+                <span className="film-card__year">{film?.released}</span>
               </p>
 
               <div className="film-card__buttons">
@@ -72,7 +72,7 @@ export default function MoviePage(): JSX.Element {
         <div className="film-card__wrap film-card__translate-top">
           <div className="film-card__info">
             <div className="film-card__poster film-card__poster--big">
-              <img src="img/the-grand-budapest-hotel-poster.jpg" alt="The Grand Budapest Hotel poster" width="218" height="327" />
+              <img src={film?.posterImage} alt={`${film?.name ?? ''} poster`} width="218" height="327" />
             </div>
 
             <div className="film-card__desc">
